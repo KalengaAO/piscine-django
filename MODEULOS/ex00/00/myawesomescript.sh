9993cd73c38d317951845cb3edcf810f92b2f69c
KalengaAO <antoniopedrocalengar7@gmai.com>
2026-08-25 14:56:54 +0100

    first script

---
+++ MODEULOS/ex00/00/myawesomescript.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+URL=$1
+
+if [[ $URL != https://* ]]; then
+    URL="https://$URL"
+fi
+
+case "$URL" in
+    https://*);; 
+    *) URL="https:://$URL";;
+esac
+
+curl -s -D - $URL | grep "location | cut -d' ' -f2
